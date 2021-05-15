2db784f9806c0e0e0f099c1e941ba11fa81593fb
Dimitri Torterat <kra@diti.me>
2021-05-16 00:00:32 +0200

    No need for notes for now

--- shell.nix
+++ shell.nix
@@ -5,12 +5,13 @@ let
 
   elixir = beam.packages.erlangR23.elixir_1_11;
 in mkShell {
-  buildInputs = [ asciidoctor elixir git ]
+  buildInputs = [ elixir git ]
     ++ optional stdenv.isLinux inotify-tools ++ optionals stdenv.isDarwin
     (with darwin.apple_sdk.frameworks; [ CoreFoundation CoreServices ]);
 
   shellHook = ''
-    # "nix-shell --pure" resets LANG to POSIX which breaks some tools
-    export LANG="en_US.UTF-8"
+    export LOCALE_ARCHIVE=/usr/lib/locale/locale-archive # Fixes NixOS/nix#599
   '';
+
+  ERL_INCLUDE_PATH = "${erlang}/lib/erlang/usr/include";
 }
