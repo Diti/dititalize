0b14eeb6c60fe6c29a22265b43c7c48e7989b91d
Dimitri Torterat <kra@diti.me>
2021-05-16 00:46:48 +0200

    New umbrella project

--- shell.nix
+++ shell.nix
@@ -5,13 +5,14 @@ let
 
   elixir = beam.packages.erlangR23.elixir_1_11;
 in mkShell {
-  buildInputs = [ elixir git ]
+  buildInputs = [ asciidoctor elixir git ]
     ++ optional stdenv.isLinux inotify-tools ++ optionals stdenv.isDarwin
     (with darwin.apple_sdk.frameworks; [ CoreFoundation CoreServices ]);
 
   shellHook = ''
-    export LOCALE_ARCHIVE=/usr/lib/locale/locale-archive # Fixes NixOS/nix#599
+    SOURCE_DATE_EPOCH = "${git} log -1 --pretty=%ct";
   '';
 
   ERL_INCLUDE_PATH = "${erlang}/lib/erlang/usr/include";
+  LOCALE_ARCHIVE = "/usr/lib/locale/locale-archive"; # Fixes NixOS/nix#599
 }
